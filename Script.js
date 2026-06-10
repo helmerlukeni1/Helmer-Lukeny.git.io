d12f70d8281ef652d20f0131c1644d5593ec802b
helmerlukeni1 <helmerlukeni1@gmail.com>
2026-06-10 23:49:56 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,12 @@
+let nome= prompt("Por favor digita seu nome:")
+if(nome) {
+  alert(" Olá," + nome + "! Seja bem-vindo. Deus te ama, campeão! ⭐'");
+} 
+
+function alertHello() {
+  alert("Mateus 19:14 | Jesus, porém, disse: Deixai os pequeninos e não os estorveis de vir a mim, porque dos tais é o Reino dos céus.")
+}
+
+
+document.getElementById("texto").innerHTML= soma
+
